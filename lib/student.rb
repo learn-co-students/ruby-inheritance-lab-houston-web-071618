3b5fa1bd5ac72ff00caeafbb2ddb78e4d3354f46
AngelaDiSalvo <angela.disalvo@gmail.com>
2018-07-21 12:56:30 -0500

    oo inheritance lab mod1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge_string)
+    self.knowledge << knowledge_string
+  end
 
 end
